7396ec9a1b999ecea2fcaca9c6b9030bd926df79
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    sdm710-common: Split component-overrides to support pure AOSP/QTI BT stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -13,7 +13,8 @@ $(call inherit-product-if-exists, vendor/realme/sdm710-common/sdm710-common-vend
 
 # Component overrides
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
